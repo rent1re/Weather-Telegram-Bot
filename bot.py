747439879ed95f8371d59517b8b58181f58eb973
rent1re <ilya.medvedev.kg@gmail.com>
2026-04-30 13:26:07 +0600

    add libraries, requirement, read me

---
+++ bot.py
@@ -0,0 +1,10 @@
+import telebot
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+import requests
+from datetime import datetime, timedelta
+
+
+BOT_TOKEN = "8774276245:AAF5xLq0I1TQBqPijDydlo58WuO2KrVU4I4"
+ 
+bot = telebot.TeleBot(BOT_TOKEN)
+ 
